af724fcd33610270502771b5dccf985a7a3b9b80
storedeeb2020-dot <storedeeb2020@gmail.com>
2026-08-09 05:46:12 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function OfflinePage() {
+  return (
+    <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center', background: '#000', color: '#fff', fontFamily: 'sans-serif' }}>
+      <h1>Site Offline</h1>
+    </div>
+  );
+}
